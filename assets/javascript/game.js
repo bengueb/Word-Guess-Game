276e70a8dc5a064bacbcf5782d5152fd0caf0b06
Elsa Bengu <elsabengu@att.net>
2019-09-11 19:48:25 -0700

    Added the javascript regarding my word choices and the current word.

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+var wordGuess = {
+
+    wordChoices: {
+        beyonce:{},
+        diana:{},
+        motherteresa:{},
+        oprah:{},
+        ruthbaderginsburg:{},
+        halsey:{},
+        gloriasteinem:{},
+        malala:{},
+        rosaparks:{},
+        bettywhite:{},
+    }
+
+    currentWord: null,
+    lettersOfTheWord: [],
+    correctLetters:[],
+    guessedLetters:[],
+    guessesLeft: 0,
+    totalGuesses: 0,
+    lettersGuessed: null,
+    wins:0,
+
+    setupGame: function(){
+        
+    }
+};

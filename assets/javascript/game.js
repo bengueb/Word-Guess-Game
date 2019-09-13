7834bb6fb894bdac5de42727a18828a4b3e88f47
Elsa Bengu <elsabengu@att.net>
2019-09-13 15:58:52 -0700

    Added the upadate wins section and pictures to the images folder.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -90,6 +90,49 @@ var wordGuess = {
 
     restartGame: function() {
         document.querySelector("#letters-guessed").innerHTML = "";
+
+        this.currentWord = null;
+        this.lettersOfTheWord = [];
+        this.correctLetters = [];
+        this.guessedLetters = [];
+        this.guessesLeft = 0;
+        this.totalGuesses = 0;
+        this. lettersGuessed = null;
+        this.setupGame();
+        this.rebuildWordView();
+    },
+
+    updateWins: function (){
+        var win;
+
+        if(this.correctLetters.length === 0){
+            win = false;
+        }
         
+        else{
+            win = true;
+        }
+
+        for (var i=o; i<this.lettersOfTheWord.length; i++) {
+            if (this.correctLetters.indexOf(this.lettersOfTheWord[i]) === -1) {
+                win = false;
+            }
+        }
+
+        if (win){
+            this.wins = this.wins + 1;
+            document.querySelector("#wins").innerHTML = this.wins;
+
+            return true;
+        }
+        return false;
+    }
+};
+
+wordGuessGame.setupGame ();
+document.onupkey = function(event){
+    if (event.keyCode >= 49 && event.keyCode <= 90){
+        wordGuessGame.lettersGuessed = event.key.toLowerCase();
+        wordGuessGame.updatePage(wordGuessGame.lettersGuessed);
     }
 };
